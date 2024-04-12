f8b43bd4cc340fa6f09963adb52b6a883afb6b5c
Igone van_der_Kley <igone@vdkley.nl>
2024-04-12 22:48:06 +0200

    Reserver changed route in navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,7 +18,7 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ url('reserveren') }}">
+                    <x-nav-link href="{{ url('reserve') }}">
                         {{ __('Reserveren') }}
                     </x-nav-link>
                 </div>

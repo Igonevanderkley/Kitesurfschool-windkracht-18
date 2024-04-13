6d2b94e12bdd61b218fa9c61b361451e4cf13e43
Igone van_der_Kley <igone@vdkley.nl>
2024-04-13 03:05:36 +0200

    Reservation form updated

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -27,37 +27,60 @@
             </ul>
 
             <div class="form">
-                <form action="">
-                    <div>
-                        <x-input-label for="location" :value="__('Les locatie')" />
-                        <x-text-input id="location" name="location" type="text" class="mt-1 block w-full" />
+                <form action="{{ route('ReservationController.store') }}" method="POST">
+                    @csrf
+                    <div class="basic-data">
+
+
+                        <div>
+                            <x-input-label for="location" :value="__('Les locatie')" />
+                            <select name="location" id="location">
+                                <option value="Zandvoort">Zandvoort</option>
+                                <option value="Muiderberg">Muiderberg</option>
+                                <option value="Wijk aan Zee">Wijk aan Zee</option>
+                                <option value="Ijmuiden">Ijmuiden</option>
+                                <option value="Scheveningen">Scheveningen</option>
+                                <option value="Hoek van Holland">Hoek van Holland</option>
+                            </select>
+                        </div>
+
+                        <div>
+                            <x-input-label for="date" :value="__('Datum')" />
+                            <input type="date" id="date" name="date">
+                        </div>
+
+                        <div>
+                            <x-input-label for="time" :value="__('Tijdslot')" />
+                            <input type="time" id="time" name="time">
+                        </div>
                     </div>
 
-                    <div>
-                        <x-input-label for="location" :value="__('Les locatie')" />
-                        <x-text-input id="location" name="location" type="datetime-local" min="" max="17:00"
-                            class="mt-1 block w-full" />
-                    </div>
+
                     @if ($chosenPackage->persons > 1)
                         <h2>duo-lesser informatie</h2>
                         <div>
-                            <x-input-label for="name" :value="__('Naam')" />
-                            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" />
+                            <x-input-label for="secondary_person_name" :value="__('Naam')" />
+                            <x-text-input id="secondary_person_name" name="secondary_person_name" type="text" class="mt-1 block w-full" />
                         </div>
 
                         <div>
-                            <x-input-label for="street" :value="__('Straat & huisnummer')" />
-                            <x-text-input id="street" name="street" type="text" class="mt-1 block w-full" />
+                            <x-input-label for="secondary_person_street" :value="__('Straat & huisnummer')" />
+                            <x-text-input id="secondary_person_street" name="secondary_person_street" type="text" class="mt-1 block w-full" />
                         </div>
 
                         <div>
-                            <x-input-label for="hometown" :value="__('Woonplaats')" />
-                            <x-text-input id="hometown" name="hometown" type="text" class="mt-1 block w-full" />
+                            <x-input-label for="secondary_person_hometown" :value="__('Woonplaats')" />
+                            <x-text-input id="secondary_person_hometown" name="secondary_person_hometown" type="text" class="mt-1 block w-full" />
                         </div>
                     @endif
+                    <input type="hidden" name="package_id" value="{{ $chosenPackage->id }}">
+
+                    <button class="blue-button" type="submit">Reserveer</button>
                 </form>
             </div>
         </div>
+        </div>
+
     </x-app-layout>
 </body>
 

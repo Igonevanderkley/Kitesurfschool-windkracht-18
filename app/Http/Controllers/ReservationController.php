be75ed99ceff5499b0f45fb15b2a740054a2a656
Igone van_der_Kley <igone@vdkley.nl>
2024-04-13 03:01:49 +0200

    Reserve functionality added to controller

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Packages;
+use Illuminate\Http\Request;
+use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
@@ -11,7 +13,40 @@ class ReservationController extends Controller
         $chosenPackage = Packages::find($packageId);
 
         return view('reserve', [
-            'chosenPackage' => $chosenPackage   
+            'chosenPackage' => $chosenPackage
         ]);
+    }
+
+    public function store(Request $request)
+    {
+
+
+        $request->validate([
+            'location' => ['required'],
+            'date' => ['required', 'date'],
+            'time' => ['required'],
+            'secondary_person_name' => [ 'max:255', 'string'],
+            'secondary_person_street' => ['max:255', 'string'],
+            'secondary_person_hometown' => ['max:255', 'string'],
+        ]);
+
+        $reservation = new Reservation();
+
+        $reservation->user_id = auth()->id();
+        $reservation->package_id = $request->package_id;
+        $reservation->location = $request->location;
+        $reservation->date = $request->date;
+        $reservation->time = $request->time;
+
+
+        if ($request->package_id > 1) {
+            $reservation->secondary_person_name = $request->secondary_person_name;
+            $reservation->secondary_person_street = $request->secondary_person_street;
+            $reservation->secondary_person_hometown = $request->secondary_person_hometown;
+        }
+        $reservation->is_paid = false;
+        $reservation->save();
+
+        return redirect()->route('/reservations');
     }
 }
